9061f902eafd5ff8c74dd8843b49d8a20573e3f9
Sebastian Köhler <sebastiankoehler2@gmx.net>
2016-11-26 21:14:30 +0000

    added the controller for sending a request

---
+++ sound/app/controllers/readingjob.js
@@ -0,0 +1,33 @@
+var dir = require('../../config/dir.js');
+var cons = require('../../config/cons.js');
+var ext = require('../../config/ext.js');
+
+module.exports = function() {
+  var cron = require('cron');
+  var cronJob = cron.job("*/5 * * * * *", function(){
+      readFile();
+  });
+  cronJob.start();
+}
+
+function readFile() {
+
+    var filePath = dir.SOUND + cons.FILE_NAME;
+    console.log(filePath);
+
+    ext.fs.readFile(filePath, (err, data) => {
+      if (err) throw err;
+      sendFileToBrain(data);
+    });
+}
+
+function sendFileToBrain(data) {
+
+  unirest.post(cons.BRAIN_URL)
+          .headers({'Accept': 'application/json'})
+          .send(data)
+          .end(function (response) {
+          console.log(response.body);
+      });
+
+}
