09656a58c1b624bd1dcc9caf708b685d16d2c36d
Tilaxx <marekhubbell@yahoo.com>
2016-11-26 21:28:54 +0000

    added mic folder

---
+++ mic/get_recordings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SEGMENT_LENGTH=$1
+NUMBER_OF_SEGMENTS=$2
+i=0
+while [ $i -lt $NUMBER_OF_SEGMENTS ]
+do
+	arecord -D plughw:1 --duration $SEGMENT_LENGTH -f cd -vv ~/record.wav
+	i=$((i + 1))
+	echo "segment number"
+	echo $i
+done
+
