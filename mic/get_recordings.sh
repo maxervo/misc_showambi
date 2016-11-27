a79e5821faa596885c845c583f4ef28f9e9e63b7
Tilaxx <marekhubbell@yahoo.com>
2016-11-27 00:10:41 +0000

    now mono

--- mic/get_recordings.sh
+++ mic/get_recordings.sh
@@ -4,7 +4,7 @@ NUMBER_OF_SEGMENTS=$2
 i=0
 while [ $i -lt $NUMBER_OF_SEGMENTS ]
 do
-	arecord -D plughw:1 --duration $SEGMENT_LENGTH -f cd -vv ~/record.wav
+	arecord -D plughw:1 --duration $SEGMENT_LENGTH -c 1 -f cd -vv ~/record.wav
 	i=$((i + 1))
 	echo "segment number"
 	echo $i
