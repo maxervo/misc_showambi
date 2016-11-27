d6200bdc7441131c82fae638b6fc5b3f370507f3
Tilaxx <marekhubbell@yahoo.com>
2016-11-27 01:49:44 +0000

    44kHz

--- mic/get_recordings.sh
+++ mic/get_recordings.sh
@@ -4,7 +4,7 @@ NUMBER_OF_SEGMENTS=$2
 i=0
 while [ $i -lt $NUMBER_OF_SEGMENTS ]
 do
-	arecord -D plughw:1 -c 1  --duration $SEGMENT_LENGTH -f U8 -vv ~/record.wav
+	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r44100 -vv ~/record.wav
 	i=$((i + 1))
 	echo "segment number"
 	echo $i
