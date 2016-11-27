74c8c8a5dc48b5aaf919f6630e7df088f1243f56
Tilaxx <marekhubbell@yahoo.com>
2016-11-27 11:25:00 +0000

    added file joining and printing of amplitude

--- mic/get_amplitude.sh
+++ mic/get_amplitude.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sox ~/record.wav -r 44 record_amps_small.dat &
+READ_FILENAME=$1
+sox $READ_FILENAME -r 1 record_amps_small.dat
 echo "new amplitudes file"
 
 
--- mic/get_recordings.sh
+++ mic/get_recordings.sh
@@ -4,7 +4,17 @@ NUMBER_OF_SEGMENTS=$2
 i=0
 while [ $i -lt $NUMBER_OF_SEGMENTS ]
 do
-	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r44100 -vv ~/record.wav
+	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record0.wav
+	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record1.wav
+	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record2.wav
+	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record3.wav
+	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record4.wav
+	arecord	-D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record5.wav
+	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record6.wav
+	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record7.wav
+	arecord	-D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record8.wav
+	arecord -D plughw:1 -c 1 --duration $SEGMENT_LENGTH -f U8 -r22050 -vv ~/record9.wav
+	sox ~/record0.wav ~/record0.wav ~/record1.wav ~/record2.wav ~/record3.wav ~/record4.wav ~/record5.wav ~/record6.wav ~/record7.wav ~/record8.wav ~/record9.wav ~/record.wav
 	i=$((i + 1))
 	echo "segment number"
 	echo $i
